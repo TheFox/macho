f3d254def7348d703cc617213758a1cde68a76b2
Christian Mayer <christian@fox21.at>
2016-11-11 10:15:51 +0100

    Check variable removed; exit on catch().

--- examples/example1.php
+++ examples/example1.php
@@ -9,29 +9,24 @@ $binary = new Binary('example1');
 $binary->setExpectedFileSize(8496); // Optional
 $binary->setExpectedMd5sum('ac0ebae5df127191a003b09a700390bc'); // Optional
 
-
-$check = false;
 try{
 	$binary->analyze();
-	$check = true;
 }
 catch(Exception $e){
 	print 'ERROR: '.$e->getMessage().PHP_EOL;
+	exit(1);
 }
+print 'check OK'.PHP_EOL;
 
-if($check){
-	print 'check OK'.PHP_EOL;
-	
-	// Example 1
-	// In this example we want to overwrite the frist line of
-	// the original source c code: puts("hello world");
-	// In this case we want to overwrite two instructions:
-	// 		leaq   0x37(%rip), %rdi
-	// 		callq  0x100000f60
-	// This two instructions are 12 byte long.
-	$sectionBaseAddress = 0x0000000100000f40;
-	$cmdAddress = 0x0000000100000f44;
-	$offset = $cmdAddress - $sectionBaseAddress;
-	$len = 12;
-	$binary->write('__TEXT', '__text', $offset, pack('H*', str_repeat('90', $len)));
-}
+// Example 1
+// In this example we want to overwrite the frist line of
+// the original source c code: puts("hello world");
+// In this case we want to overwrite two instructions:
+// 		leaq   0x37(%rip), %rdi
+// 		callq  0x100000f60
+// This two instructions are 12 byte long.
+$sectionBaseAddress = 0x0000000100000f40;
+$cmdAddress = 0x0000000100000f44;
+$offset = $cmdAddress - $sectionBaseAddress;
+$len = 12;
+$binary->write('__TEXT', '__text', $offset, pack('H*', str_repeat('90', $len)));
--- examples/example2.php
+++ examples/example2.php
@@ -9,27 +9,22 @@ $binary = new Binary('example2');
 $binary->setExpectedFileSize(8496); // Optional
 $binary->setExpectedMd5sum('ac0ebae5df127191a003b09a700390bc'); // Optional
 
-
-$check = false;
 try{
 	$binary->analyze();
-	$check = true;
 }
 catch(Exception $e){
 	print 'ERROR: '.$e->getMessage().PHP_EOL;
+	exit(1);
 }
+print 'check OK'.PHP_EOL;
 
-if($check){
-	print 'check OK'.PHP_EOL;
-	
-	// Example 2
-	// We want to change the text "a second line" from the second line
-	// of code into "2nd line". The length of the new string can't be
-	// longer as the original string. Consider to close the string
-	// with a "\0" null char.
-	$sectionBaseAddress = 0x0000000100000f82;
-	$cmdAddress = 0x0000000100000f8e;
-	$offset = $cmdAddress - $sectionBaseAddress;
-	$len = 12;
-	$binary->write('__TEXT', '__cstring', $offset, '2nd line'.chr(0));
-}
+// Example 2
+// We want to change the text "a second line" from the second line
+// of code into "2nd line". The length of the new string can't be
+// longer as the original string. Consider to close the string
+// with a "\0" null char.
+$sectionBaseAddress = 0x0000000100000f82;
+$cmdAddress = 0x0000000100000f8e;
+$offset = $cmdAddress - $sectionBaseAddress;
+$len = 12;
+$binary->write('__TEXT', '__cstring', $offset, '2nd line'.chr(0));
